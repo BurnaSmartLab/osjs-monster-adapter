7a49e4885541cfacddba9e3d21fb0459a1203f35
Seyed Milad Hashemi <s.milad.hashemi@gmail.com>
2020-07-12 14:32:07 +0430

    initial commit

---
+++ src/Common.js
@@ -0,0 +1,8 @@
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest; ////XMLHTTPRequest object must generated each time
+const axios = require('axios')
+exports.Common = class Common extends XMLHttpRequest {
+    constructor() {
+        super();
+    }
+}
+exports.axios = axios
